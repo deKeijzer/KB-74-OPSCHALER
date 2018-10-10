7e78f5caf44b29fbda1f5cb1ca745c3b3baa2cf9
PoldeVisser <poldevisser@gmail.com>
2018-10-10 13:00:10 +0200

    finished first def and made readme

--- Opschaler.py
+++ Opschaler.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 
 # In[2]:
 
-def dwel_path_id(sample_rate, map_):
+def dwel_path_id(sample_rate, folder, combined):
     """
     Reads in the file paths and dwelling id's of the combined smartmeter data.
     :return: file_paths, dwelling_ids, both as lists.
@@ -19,13 +19,16 @@ def dwel_path_id(sample_rate, map_):
     path = '//datc//opschaler//combined_gas_smart_weather_dfs//'
     map_ = 'processed//'
     subscript = '_hour'
+    combined_ = 'P*'
 
     if sample_rate == '10s':
         subscript = '_10s'
-    if map_ == 'unprocessed':
+    if folder == 'unprocessed':
         map_ = 'unprocessed//'
+    if combined == 'yes':
+        combined_ = 'all_dwellings_combined'
         
-    complete_path = path+map_+'*'+subscript+".csv"
+    complete_path = path+map_+combined_+subscript+".csv"
     file_paths = np.array(glob.glob(complete_path))
 
     print('complete_path: '+complete_path)
@@ -35,6 +38,8 @@ def dwel_path_id(sample_rate, map_):
     
     if sample_rate == '10s':
         dwelling_ids = np.array(list((map(lambda x: x[-19:-8], file_paths)))) # 10s ids slicing
+    if combined == 'yes':
+        dwelling_ids = 'Used all dwellings to make this df'
     
     
     return file_paths, dwelling_ids
@@ -48,14 +53,10 @@ def dwel_path_id(sample_rate, map_):
 
 
 
-
-
-def dwel_len(type_, dwelling_id=None):
+def dwel_len(type_, dwelling_id):
     """
     Get the total amount of days of the unprocessed dwelling_id where type=10s/h.
     """
-    if dwelling_id is None:
-        dwelling_id = op.dwelling_id_list()
     dir = '//datc//opschaler//combined_gas_smart_weather_dfs//unprocessed//'
     df = pd.read_csv(dir+dwelling_id+'_'+type_+'.csv', delimiter='\t', parse_dates=['datetime'])
     columns = df.columns
