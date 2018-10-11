a410a2c3d84bd08cf6cad1097b5f07716ae29ba2
PoldeVisser <poldevisser@gmail.com>
2018-10-11 12:54:37 +0200

    making def for corr matrix

--- Opschaler.py
+++ Opschaler.py
@@ -7,6 +7,8 @@ import numpy as np
 import glob
 import pandas as pd
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 # In[2]:
@@ -42,7 +44,7 @@ def dwel_path_id(sample_rate, folder, combined):
         dwelling_ids = 'Used all dwellings to make this df'    
     
     return file_paths, dwelling_ids
-    
+    file_paths, dwelling_ids = op.dwel_path_id('sample_rate', 'folder', 'combined')
 # def nan_table(sample_rate):
 #     file_paths, dwelling_ids = dwelling_data_paths(sample_rate)
 #     dfs_nan_table = []
@@ -57,12 +59,41 @@ def dwel_path_id(sample_rate, folder, combined):
 #     return final_df
     
 
+def cor_df(len_measurement, dwelling_id):
+    %matplotlib inline
+    plt.style.use('default')
 
+    df = pd.read_csv("/datc/opschaler/combined_gas_smart_weather_dfs/processed/"+dwelling_id+"_hour.csv",header=0,delimiter="\t",parse_dates = ['datetime'])
+    df = df.set_index(['datetime'])
 
+    if len_measurement == '3h':
+        df = df.resample('3H').mean()
+    if len_measurement == '6h':
+        df = df.resample('6H').mean()
+    if len_measurement == '12h':
+        df = df.resample('12H').mean()
+    if len_measurement == '1d':
+        df = df.resample('1D').mean()
+    if len_measurement == '1w':
+        df = df.resample('1W').mean()
+    if len_measurement == '1m':
+        df = df.resample('1M').mean()
+    
+    rdf = df[df['T'] < 16]
+    
+    corr = rdf.corr()
+    mask = np.zeros_like(corr)
+    mask[np.triu_indices_from(mask)] = True
 
+    fig, ax = plt.subplots(figsize=(25,25))
 
+    sns.heatmap(corr, mask=mask, square=False, cmap='RdYlGn', annot=True, ax=ax,
+                cbar_kws={'label':'Pearson correlation coefficient [-]'})
 
-
+    plt.title('Correlation Matrix')
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+    plt.show()
 
 
 
