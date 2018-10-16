2f729d04b1f0fe9cb3a5a27298dee012566c71d5
PoldeVisser <poldevisser@gmail.com>
2018-10-16 11:34:48 +0200

    making a main function for merging dfs etc

--- Opschaler.py
+++ Opschaler.py
@@ -9,8 +9,7 @@ import pandas as pd
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-global sr
+import time
 
 # In[2]:
 
@@ -93,6 +92,80 @@ def dwel_path_id(sample_rate, folder, combined):
         dwelling_ids = 'Used all dwellings to make this df'    
     
     return file_paths, dwelling_ids
+
+def clean_datetime(df):
+    """
+    Input should be a df with a column called 'datetime'.
+    This function checks wether a row in the df.datetime column can be parsed to a pandas datetime object,
+    by trying pd.to_datetime() on it.
+    If it fails it will replace that row with np.nan().
+    Finally this function will return the df with the NaN rows dropped.
+    It only drops the row if the datetime column contains a NaN.
+    """
+    for i in range(len(df)):
+        try:
+            pd.to_datetime(df.datetime[i])
+        except ValueError:
+            print('-----')
+            print('ValueError at index = %s' % i)
+            print(df.datetime[i])
+            df.datetime = df.datetime.replace(df.datetime[i], np.nan)
+    df = df.dropna(subset = ['datetime'])
+    return df
+
+def clean_prepare_smart_gas(file_path):
+    """
+    Input is a dwelling_id.csv file.
+    Output are cleaned & prepared dataframes (smart, gas).
+    Return: smart, gas
+    """
+    df = pd.read_csv(file_path, delimiter=';', header=0)
+    smart = df.iloc[:,:7]
+    gas = df.iloc[:,7:]
+    
+    smart = smart.rename(index=str,columns={"Timestamp":"datetime"})
+    gas = gas.rename(index=str,columns={"gasTimestamp":"datetime"})
+
+    smart = clean_datetime(smart)
+    gas = clean_datetime(gas)
+    
+    smart['datetime'] = pd.to_datetime(smart['datetime'])
+    gas['datetime'] = pd.to_datetime(gas['datetime'])
+
+    
+    smart = smart.set_index(['datetime'])
+    gas = gas.set_index(['datetime'])
+
+    return smart, gas
+
+def resample_smart_gas(smart, gas):
+    """
+    Resamples the (smart, gas) dfs to 10s.
+    Also calculates gasPower. 
+    Returns (smart_resampled, gas_resampled)
+    """
+    smart_resampled = smart.resample('10s').mean()
+    
+    gas_resampled = gas.resample('H').mean()
+    # replace 0s with NaNs
+    gas_resampled = gas_resampled.resample('10s').interpolate(method='time')
+    gas_resampled['gasPower'] = gas_resampled['gasMeter'].diff()
+    
+    return smart_resampled, gas_resampled
+
+def merge_smart_gas_weather(smart_resampled, gas_resampled, weather):
+    """
+    Merges the dataframes, outputs one df.
+    """
+    df = pd.merge(smart_resampled, gas_resampled, left_index=True, right_index=True)
+    df = pd.merge(df, weather, left_index=True, right_index=True)
+    
+    return df
+
+def save_df(df, dwelling_id):
+    dir = '//datc//opschaler//combined_dfs_gas_smart_weather//'
+    df.to_csv(dir+dwelling_id+'.csv', sep='\t', index=True)
+    print('Saved %s' % dwelling_id)
 # def nan_table(sample_rate):
 #     file_paths, dwelling_ids = dwelling_data_paths(sample_rate)
 #     dfs_nan_table = []
