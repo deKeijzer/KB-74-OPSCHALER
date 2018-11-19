a2d8f9cdf0110998a071220af636cfb0e3fe35c4
deKeijzer <briandekeyzer@hotmail.com>
2018-11-19 19:14:40 +0100

    Implemented Keras learning rate finder. It currently is only used in the CNN notebook.

--- Personal_folders/Brian/Machine & Deep learning/keijzer.py
+++ Personal_folders/Brian/Machine & Deep learning/keijzer.py
@@ -265,3 +265,76 @@ def df_to_lstm_format(df, test_size=0.5, look_back=5, target_column='target', sc
         y_test_reshaped[i] = y_test[y_position]
     
     return X_train_reshaped, y_train_reshaped, X_test_reshaped, y_test_reshaped
+
+from keras.callbacks import Callback
+import keras.backend as K
+
+class LR_Finder(Callback):
+    """
+    This function is NOT made by me.
+    Source: http://puzzlemusa.com/2018/05/14/learning-rate-finder-using-keras/
+    """
+    def __init__(self, start_lr=1e-5, end_lr=10, step_size=None, beta=.98):
+        super().__init__()
+        
+        self.start_lr = start_lr
+        self.end_lr = end_lr
+        self.step_size = step_size
+        self.beta = beta
+        self.lr_mult = (end_lr/start_lr)**(1/step_size)
+        
+    def on_train_begin(self, logs=None):
+        self.best_loss = 1e9
+        self.avg_loss = 0
+        self.losses, self.smoothed_losses, self.lrs, self.iterations = [], [], [], []
+        self.iteration = 0
+        logs = logs or {}
+        K.set_value(self.model.optimizer.lr, self.start_lr)
+        
+    def on_batch_end(self, epoch, logs=None):
+        logs = logs or {}
+        loss = logs.get('loss')
+        self.iteration += 1
+        
+        self.avg_loss = self.beta * self.avg_loss + (1 - self.beta) * loss
+        smoothed_loss = self.avg_loss / (1 - self.beta**self.iteration)
+        
+        # Check if the loss is not exploding
+        if self.iteration>1 and smoothed_loss > self.best_loss * 4:
+            self.model.stop_training = True
+            return
+
+        if smoothed_loss < self.best_loss or self.iteration==1:
+            self.best_loss = smoothed_loss
+        
+        lr = self.start_lr * (self.lr_mult**self.iteration)
+        
+        self.losses.append(loss)
+        self.smoothed_losses.append(smoothed_loss)
+        self.lrs.append(lr)
+        self.iterations.append(self.iteration)
+        
+        
+        K.set_value(self.model.optimizer.lr, lr)  
+        
+    def plot_lr(self):
+        plt.xlabel('Iterations')
+        plt.ylabel('Learning rate')
+        plt.plot(self.iterations, self.lrs)
+        
+    def plot(self, n_skip=10):
+        plt.ylabel('Loss')
+        plt.xlabel('Learning rate (log scale)')
+        plt.plot(self.lrs[n_skip:-5], self.losses[n_skip:-5])
+        plt.xscale('log')
+        
+    def plot_smoothed_loss(self, n_skip=10):
+        plt.ylabel('Smoothed Losses')
+        plt.xlabel('Learning rate (log scale)')
+        plt.plot(self.lrs[n_skip:-5], self.smoothed_losses[n_skip:-5])
+        plt.xscale('log')
+        
+    def plot_loss(self):
+        plt.ylabel('Losses')
+        plt.xlabel('Iterations')
+        plt.plot(self.iterations[10:], self.losses[10:])
